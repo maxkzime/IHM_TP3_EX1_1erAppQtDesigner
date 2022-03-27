ed0de9d0579948860b00f34c4fada6200f9ffdfb
maxkzime <98185988+maxkzime@users.noreply.github.com>
2022-03-27 22:30:39 +0200

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -1,3 +1,8 @@
+// TP3/EX1/widget.cpp
+// Exercice 1 : première application avec QtDesigner
+// BODIN Maxime C2
+// 22/03/27
+
 #include "widget.h"
 #include "ui_widget.h"
 
@@ -6,9 +11,7 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::IHM)
-{
-    ui->setupUi(this);
-}
+{ui->setupUi(this);}
 
 Widget::~Widget()
 {delete ui;}
